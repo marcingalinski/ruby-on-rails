c0265c78ea75328e0d2a0b9c7c1f127045e6f30d
Marcin Galiński <marcin.galinski95@gmail.com>
2015-11-13 18:18:54 +0100

    finished blog comments feature

--- blogger/app/controllers/application_controller.rb
+++ blogger/app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def show
   	@article = Article.find(params[:id])
+  	@comment = Comment.new
+	@comment.article_id = @article.id
+
 	end
 
 end
